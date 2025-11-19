4513a282fcae79c2982cc7214c1ab5d9501af002
Henry McMullin <hmcmullin@student.cvtc.edu>
2025-11-18 23:04:00 -0600

    Add tests for add and search servlets

--- src/test/edu.cvtc.hooked/MainTest.java
+++ src/test/edu.cvtc.hooked/MainTest.java
@@ -1,12 +1,15 @@
 package edu.cvtc.hooked;
 
+import edu.cvtc.hooked.dao.CatchDao;
+import edu.cvtc.hooked.model.Catch;
 import edu.cvtc.hooked.model.SpeciesRestrictions;
 import edu.cvtc.hooked.util.DbUtil;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.sql.*;
- 
+import java.util.List;
+
 public class MainTest {
     @Test
     void getConnection_opensAndCloses() throws Exception {
@@ -32,7 +35,7 @@ public class MainTest {
     @Test
     void restrictions() throws SQLException, ClassNotFoundException {
         try (Connection db = DbUtil.getConnection()) {
-            String speciesStr     = "bluegill";
+            String speciesStr = "bluegill";
             double length = 20.00;
             double weight = 10.00;
 
@@ -41,6 +44,183 @@ public class MainTest {
 
             boolean invalid = (length > restrictions.getMaxLength()) || (weight > restrictions.getMaxWeight());
             assertTrue(invalid);
+        }
+    }
+
+    @Test
+    void dbinput() throws SQLException, ClassNotFoundException {
+        try (Connection c = DbUtil.getConnection();
+             Statement stmt = c.createStatement()) {
+            stmt.executeUpdate("DROP TABLE IF EXISTS Catches");
+            stmt.executeUpdate("DROP TABLE IF EXISTS Users");
+        }
+        DbUtil.ensureSchema();
+        try (Connection db = DbUtil.getConnection()) {
+            String speciesStr = "bluegill";
+            String locationStr = "mississippi river";
+            String baitStr = "gulp! minnow";
+            String dateCaught = "2025-9-15";
+            String notes = "slip bobber";
+            String lengthStr = "11.0";
+            String weightStr = "1.0";
+
+            Integer userID = 1;
+            String firstName = "Test";
+            String lastName = "User";
+            String username = "TestUser";
+            String password = "Test";
+
+            String user = "INSERT INTO Users (firstName, lastName, userName, passwordHash) VALUES (?, ?, ?, ?)";
+
+            int userId;
+            try (PreparedStatement ps1 = db.prepareStatement(user, Statement.RETURN_GENERATED_KEYS)) {
+                ps1.setString(1, firstName);
+                ps1.setString(2, lastName);
+                ps1.setString(3, username);
+                ps1.setString(4, password);
+
+                int res = ps1.executeUpdate();
+                System.out.println("User rows inserted: " + res);
+
+                ResultSet rs = ps1.getGeneratedKeys();
+                rs.next();
+                userId = rs.getInt(1);
+            }
+
+            String sql = "INSERT INTO Catches (UserID, SpeciesName, LocationName, BaitType, DateCaught, Notes, Length, Weight) " +
+                    "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+            try (PreparedStatement ps = db.prepareStatement(sql)) {
+                ps.setInt(1, (userId));
+                ps.setString(2, speciesStr);
+                ps.setString(3, locationStr);
+                ps.setString(4, baitStr);
+                ps.setString(5, dateCaught);
+                ps.setString(6, notes);
+                ps.setDouble(7, Double.parseDouble(lengthStr));
+                ps.setDouble(8, Double.parseDouble(weightStr));
+                int rows = ps.executeUpdate();
+                System.out.println("Rows inserted: " + rows);
+            }
+
+            try (PreparedStatement verifyUser = db.prepareStatement("SELECT * FROM Users WHERE UserID = ?")) {
+                verifyUser.setInt(1, userId);
+                ResultSet rs = verifyUser.executeQuery();
+
+                assertTrue(rs.next());
+                assertEquals(userID, rs.getInt("UserID"));
+                assertEquals(firstName, rs.getString("FirstName"));
+                assertEquals(lastName, rs.getString("LastName"));
+                assertEquals(username, rs.getString("Username"));
+                assertEquals(password, rs.getString("PasswordHash"));
+            }
+
+            try (PreparedStatement verifyCatch = db.prepareStatement("SELECT * FROM Catches WHERE UserID = ?")) {
+                verifyCatch.setInt(1, userId);
+                ResultSet rs = verifyCatch.executeQuery();
+
+                assertTrue(rs.next());
+                assertEquals(userID, rs.getInt("UserID"));
+                assertEquals("bluegill", rs.getString("SpeciesName"));
+                assertEquals(locationStr, rs.getString("LocationName"));
+                assertEquals(baitStr, rs.getString("BaitType"));
+                assertEquals(dateCaught, rs.getString("DateCaught"));
+                assertEquals(notes, rs.getString("Notes"));
+                assertEquals(Double.parseDouble(lengthStr), rs.getDouble("Length"));
+                assertEquals(Double.parseDouble(weightStr), rs.getDouble("Weight"));
+            }
+        }
+    }
+
+    @Test
+    void search() throws SQLException, ClassNotFoundException {
+        try (Connection c = DbUtil.getConnection();
+             Statement stmt = c.createStatement()) {
+            stmt.executeUpdate("DROP TABLE IF EXISTS Catches");
+            stmt.executeUpdate("DROP TABLE IF EXISTS Users");
+        }
+        DbUtil.ensureSchema();
+        try (Connection db = DbUtil.getConnection()) {
+            String speciesStr = "bluegill";
+            String locationStr = "mississippi river";
+            String baitStr = "gulp! minnow";
+            String dateCaught = "2025-9-15";
+            String notes = "slip bobber";
+            String lengthStr = "11.0";
+            String weightStr = "1.0";
+
+            Integer userID = 1;
+            String firstName = "Test";
+            String lastName = "User";
+            String username = "TestUser";
+            String password = "Test";
+
+            String user = "INSERT INTO Users (firstName, lastName, userName, passwordHash) VALUES (?, ?, ?, ?)";
+
+            int userId;
+            try (PreparedStatement ps1 = db.prepareStatement(user, Statement.RETURN_GENERATED_KEYS)) {
+                ps1.setString(1, firstName);
+                ps1.setString(2, lastName);
+                ps1.setString(3, username);
+                ps1.setString(4, password);
+
+                int res = ps1.executeUpdate();
+                System.out.println("User rows inserted: " + res);
+
+                ResultSet rs = ps1.getGeneratedKeys();
+                rs.next();
+                userId = rs.getInt(1);
+            }
+
+            String sql = "INSERT INTO Catches (UserID, SpeciesName, LocationName, BaitType, DateCaught, Notes, Length, Weight) " +
+                    "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+            try (PreparedStatement ps = db.prepareStatement(sql)) {
+                ps.setInt(1, (userId));
+                ps.setString(2, speciesStr);
+                ps.setString(3, locationStr);
+                ps.setString(4, baitStr);
+                ps.setString(5, dateCaught);
+                ps.setString(6, notes);
+                ps.setDouble(7, Double.parseDouble(lengthStr));
+                ps.setDouble(8, Double.parseDouble(weightStr));
+                int rows = ps.executeUpdate();
+                System.out.println("Rows inserted: " + rows);
+            }
+
+            try (PreparedStatement verifyUser = db.prepareStatement("SELECT * FROM Users WHERE UserID = ?")) {
+                verifyUser.setInt(1, userId);
+                ResultSet rs = verifyUser.executeQuery();
+
+                assertTrue(rs.next());
+                assertEquals(userID, rs.getInt("UserID"));
+                assertEquals(firstName, rs.getString("FirstName"));
+                assertEquals(lastName, rs.getString("LastName"));
+                assertEquals(username, rs.getString("Username"));
+                assertEquals(password, rs.getString("PasswordHash"));
+            }
+
+            try (PreparedStatement verifyCatch = db.prepareStatement("SELECT * FROM Catches WHERE UserID = ?")) {
+                verifyCatch.setInt(1, userId);
+                ResultSet rs = verifyCatch.executeQuery();
+
+                assertTrue(rs.next());
+                assertEquals(userID, rs.getInt("UserID"));
+                assertEquals("bluegill", rs.getString("SpeciesName"));
+                assertEquals(locationStr, rs.getString("LocationName"));
+                assertEquals(baitStr, rs.getString("BaitType"));
+                assertEquals(dateCaught, rs.getString("DateCaught"));
+                assertEquals(notes, rs.getString("Notes"));
+                assertEquals(Double.parseDouble(lengthStr), rs.getDouble("Length"));
+                assertEquals(Double.parseDouble(weightStr), rs.getDouble("Weight"));
+            }
+
+            CatchDao dao = new CatchDao();
+
+            int userIDSearch = 1;
+            List<Catch> results = dao.findByUserId(userIDSearch);
+
+            assertEquals(results.size(), 1);
         }
     }
 }
