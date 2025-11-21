6d026489388e2c1972a57a79e72a1cdff6891ffb
David Piggott <dpiggott1@student.cvtc.edu>
2025-11-20 18:04:49 -0600

    Fix UserDao: add Statement.RETURN_GENERATED_KEYS so userId is assigned for CatchDao tests

--- src/main/java/edu/cvtc/hooked/dao/UserDao.java
+++ src/main/java/edu/cvtc/hooked/dao/UserDao.java
@@ -9,12 +9,17 @@ public class UserDao {
     public void insert(User u) throws SQLException {
         String sql = "INSERT INTO Users(firstName,lastName,userName,passwordHash) VALUES(?,?,?,?)";
         try (Connection c = DbUtil.getConnection();
-             PreparedStatement ps = c.prepareStatement(sql)) {
+             PreparedStatement ps = c.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
             ps.setString(1, u.getFirstName());
             ps.setString(2, u.getLastName());
             ps.setString(3, u.getUserName());
             ps.setString(4, u.getPasswordHash());
             ps.executeUpdate();
+            try (ResultSet rs = ps.getGeneratedKeys()) {
+                if (rs.next()) {
+                    u.setUserId(rs.getInt(1));
+                }
+            }
         }
     }
 
