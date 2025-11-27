f92002b344f6f685e2870787a0fae1f356c1e2ea
David Piggott <dpiggott1@student.cvtc.edu>
2025-11-27 10:48:41 -0600

    Revert SpeciesTest

--- src/test/edu.cvtc.hooked/SpeciesTest.java
+++ src/test/edu.cvtc.hooked/SpeciesTest.java
@@ -14,10 +14,8 @@ public class SpeciesTest {
 
         assertNull(s.getSpeciesId());
         assertNull(s.getSpeciesName());
-        assertNull(s.getMinLength());
-        assertNull(s.getMaxLength());
-        assertNull(s.getMinWeight());
-        assertNull(s.getMaxWeight());
+        assertNull(s.getLength());
+        assertNull(s.getWeight());
     }
 
     @Test
@@ -27,18 +25,14 @@ public class SpeciesTest {
         Species s = new Species(
                 10,
                 "Walleye",
-                15.0,
-                30.0,
-                2.0,
+                25.0,
                 12.0
         );
 
         assertEquals(10, s.getSpeciesId());
         assertEquals("Walleye", s.getSpeciesName());
-        assertEquals(15.0, s.getMinLength());
-        assertEquals(30.0, s.getMaxLength());
-        assertEquals(2.0, s.getMinWeight());
-        assertEquals(12.0, s.getMaxWeight());
+        assertEquals(25.0, s.getLength());
+        assertEquals(12.0, s.getWeight());
     }
 
     @Test
@@ -46,18 +40,14 @@ public class SpeciesTest {
         // The new species constructor should not set an ID.
         Species s = new Species(
                 "Bass",
-                12.0,
                 20.0,
-                1.5,
                 6.0
         );
 
         assertNull(s.getSpeciesId());
         assertEquals("Bass", s.getSpeciesName());
-        assertEquals(12.0, s.getMinLength());
-        assertEquals(20.0, s.getMaxLength());
-        assertEquals(1.5, s.getMinWeight());
-        assertEquals(6.0, s.getMaxWeight());
+        assertEquals(20.0, s.getLength());
+        assertEquals(6.0, s.getWeight());
     }
 
     @Test
@@ -67,24 +57,20 @@ public class SpeciesTest {
 
         s.setSpeciesId(5);
         s.setSpeciesName("Pike");
-        s.setMinLength(25.0);
-        s.setMaxLength(45.0);
-        s.setMinWeight(2.0);
-        s.setMaxWeight(15.0);
+        s.setLength(40.0);
+        s.setWeight(15.0);
 
         assertEquals(5, s.getSpeciesId());
         assertEquals("Pike", s.getSpeciesName());
-        assertEquals(25.0, s.getMinLength());
-        assertEquals(45.0, s.getMaxLength());
-        assertEquals(2.0, s.getMinWeight());
-        assertEquals(15.0, s.getMaxWeight());
+        assertEquals(40.0, s.getLength());
+        assertEquals(15.0, s.getWeight());
     }
 
     @Test
     public void testEqualsUsesSpeciesName() {
         // Two species with the same speciesName should be equal.
-        Species a = new Species("Bluegill", 5.0, 10.0, 0.3, 1.0);
-        Species b = new Species("Bluegill", 7.0, 12.0, 0.4, 1.2);
+        Species a = new Species("Bluegill", 10.0, 1.0);
+        Species b = new Species("Bluegill", 12.0, 1.2);
 
         assertEquals(a, b);
         assertEquals(a.hashCode(), b.hashCode());
@@ -93,8 +79,8 @@ public class SpeciesTest {
     @Test
     public void testNotEqualWhenSpeciesNameDiffers() {
         // Different speciesName values should not be equal.
-        Species a = new Species("Crappie", 5.0, 8.0, 0.3, 1.2);
-        Species b = new Species("Sunfish", 5.0, 8.0, 0.3, 1.2);
+        Species a = new Species("Crappie", 5.0, 1.0);
+        Species b = new Species("Sunfish", 5.0, 1.0);
 
         assertNotEquals(a, b);
     }
@@ -105,9 +91,7 @@ public class SpeciesTest {
         Species s = new Species(
                 3,
                 "Trout",
-                10.0,
                 25.0,
-                1.0,
                 5.0
         );
 
